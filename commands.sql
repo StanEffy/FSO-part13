6d64bb502f793cba757aa62443112a2f4f25b957
Stan Effy <holydonk@gmail.com>
2022-11-14 18:01:59 +0200

    13.1 - 13-3

---
+++ commands.sql
@@ -0,0 +1,4 @@
+INSERT INTO blogs(id, author, url, title, likes);
+VALUES(0, Stephan, "www.yandex.ru", "First blog ever", 2);
+VALUES(1, Aramis,"www.gmail.com", "Another  blog", 12);
+SELECT * from blogs;
--- index.js
+++ index.js
@@ -1,16 +1,83 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, Model, DataTypes, QueryTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+  },
+});
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+let blog = sequelize.define("blogs", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      author: {
+        type: DataTypes.STRING
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      }
+}, {
+    modelName: "blogs",
+    freezeTableName: true
+})
+
+class Note extends Model {}
+
+Note.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  date: {
+    type: DataTypes.DATE
   }
-}
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'note'
+})
 
-main()
+app.get('/api/notes', async (req, res) => {
+  const notes = await Note.findAll()
+  res.json(notes)
+})
+
+
+app.get('/api/blogs', async (req, res) => {
+    const blogs = await blog.findAll()
+    res.json(blogs)
+  })
+  
+
+const PORT = process.env.PORT || 3002
+
+app.listen(PORT, () => {
+console.log(sequelize.models)
+  console.log(`Server running on port ${PORT}`)
+})
