7106c5341e4a5ead08edc3ba17f1a22fc298644a
Stan Effy <holydonk@gmail.com>
2022-11-15 13:43:49 +0200

    13.7

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser")
 const { PORT } = require('./util/config')
+
+const errorHandler = require("./util/errorHandler")
+const unknownEndpoint = require("./util/unknownEndpoint")
+
+
 const { connectToDatabase } = require('./util/db')
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 const notesRouter = require('./controllers/notes')
 const blogsRouter = require('./controllers/blogs')
 
@@ -13,6 +19,9 @@ app.use(urlencodedParser);
 
 app.use('/api/notes', notesRouter)
 app.use('/api/blogs', blogsRouter)
+app.use(unknownEndpoint)
+  
+app.use(errorHandler);
 
 const start = async () => {
   await connectToDatabase()
