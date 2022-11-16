9aeae411c1a9ad46b43874935b73d3fd4e2bcc40
Stan Effy <holydonk@gmail.com>
2022-11-16 16:12:44 +0200

    13.18

--- models/blogs.js
+++ models/blogs.js
@@ -27,6 +27,20 @@ Blog.init(
       type: DataTypes.INTEGER,
       defaultValue: 0,
     },
+    year: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				min: {
+					args: [1991],
+					msg: 'Minimum year is 1991',
+				},
+				max: {
+					args: [new Date().getFullYear()],
+					msg: `Maximum year is ${new Date().getFullYear()}`,
+				}
+			},
+		},
   },
   {
     sequelize,
